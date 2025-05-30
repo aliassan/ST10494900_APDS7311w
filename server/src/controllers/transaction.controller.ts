b2a0574168342ea59108f0fe7ead5de47aeac4ad
aliassan <alimkwananzi@gmail.com>
2025-05-30 20:48:34 +0200

    feat: add transaction and get transactions

--- server/src/controllers/transaction.controller.ts
+++ server/src/controllers/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { prisma, errorResponse, decryptWithAES } from "../utils";
+import { prisma, errorResponse, decryptWithAES, encryptWithAES } from "../utils";
 import { Request, Response } from "express";
 import crypto from "crypto";
 
@@ -13,24 +13,42 @@ export const getTransactions = async (req: Request, res: Response) => {
     //retrieve notification
     const user = await prisma.user.findUnique({
       where: {
-        accountNumber: crypto
+        accountNumber: /*crypto
           .createHash("sha256")
           .update(accountNumber)
-          .digest("hex")
+          .digest("hex")*/ accountNumber
       }
     })
 
     if (!user) return errorResponse(404, "User not found", res);
 
     if (user.isEmployee) {
-      res.status(200).json([])
+      // If the user is an employee, return all transactions
+      console.log("User is an employee, fetching all transactions");
+      const transactions = await prisma.transaction.findMany({
+        include: {
+          user: {
+            select: {
+              accountNumber: true,
+              fullName: true,
+            }
+          }
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      });
+      console.log("transactions - employee: ", transactions);
+      res.status(200).json(
+        transactions
+      );
     } else {
       const transactions = await prisma.transaction.findMany({
         where: {
           userId: user.id
         }
       })
-      console.log("transactions: ", transactions);
+      console.log("transactions - customer: ", transactions);
       res.status(200).json(transactions)
     }
   } catch (error) {
@@ -47,23 +65,45 @@ export const addTransaction = async (req: Request, res: Response) => {
     //retrieve notification
     const user = await prisma.user.findUnique({
       where: {
-        accountNumber: crypto
+        accountNumber/*: crypto
           .createHash("sha256")
           .update(accountNumber)
-          .digest("hex")
+          .digest("hex")*/
       }
     })
 
     if (!user) return errorResponse(404, "User not found", res);
 
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0);
+  
+    const todayEnd = new Date();
+    todayEnd.setHours(23, 59, 59, 999);
+  
+    // Get today's count
+    const count = await prisma.transaction.count({
+      where: {
+        createdAt: {
+          gte: todayStart,
+          lte: todayEnd,
+        },
+      },
+    });
+  
+    // Generate reference
+    const reference = `INV-${todayStart.toISOString().slice(0, 10).replace(/-/g, '')}-${String(count + 1).padStart(3, '0')}`;
+    console.log("Generated reference: ", reference);
+    console.log("transactionData: ", req.body);
+
     const transaction = await prisma.transaction.create({
       data: {
         userId: user.id,
-        ...req.body
+        ...req.body,
+        reference,
       }
     })
 
-    res.status(200).json(transaction)
+    res.status(200).json({ message: "success"})
   } catch(error) {
     return errorResponse(500, error, res)
   }
